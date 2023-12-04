bf7c67cab26902643ef9b5a6cbc44b42dd9dbb4d
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-12-04 15:20:42 +0100

    Project scaffholding

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,40 @@
+<script>
+import AppHeaderItemLinkVue from './AppHeaderItemLink.vue';
+
+
+export default {
+    components: {
+        AppHeaderItemLinkVue
+    },
+    data() {
+        return {
+            navMenu: [
+                {id: 1, text: 'Home'},
+                {id: 2, text: 'Mission'},
+                {id: 3, text: 'Causes'},
+                {id: 4, text: 'Journal'},
+            ]
+        }
+    }
+    
+}
+
+</script>
+
+<template>
+    <header>
+        <nav>
+            logoHere
+            <div>
+                <AppHeaderItemLinkVue v-for="item in navMenu" :key="item.id" :text="item.text" />
+                
+            </div>
+        </nav>
+
+    </header>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppHeaderItemLink.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    props: {
+        text: String,
+    },
+    computed: {
+        uppercaseText: function() {
+      return this.text.toUpperCase();
+        }
+    }
+}
+
+</script>
+
+<template>
+
+    <a :href="` /${text }`"> {{ uppercaseText }} </a>
+
+</template>
+
+<style lang="scss" scoped >
+
+</style>
