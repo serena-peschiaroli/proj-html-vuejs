d40d93156ed03f2a7d3742d833c88c76a85aa51a
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-12-04 16:50:02 +0100

    Add: header component: first version

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,7 +13,7 @@ export default {
                 {id: 2, text: 'Mission'},
                 {id: 3, text: 'Causes'},
                 {id: 4, text: 'Journal'},
-            ]
+            ], 
         }
     }
     
@@ -23,18 +23,48 @@ export default {
 
 <template>
     <header>
-        <nav>
-            logoHere
-            <div>
-                <AppHeaderItemLinkVue v-for="item in navMenu" :key="item.id" :text="item.text" />
+        <div class="container-xl"> 
+           <div class="nav">
+                <div class="logo">
+                    <img src="../img/avada-charity-logo.png" alt="">
+                </div>
                 
-            </div>
-        </nav>
-
+                <div class="nav-items">
+                    <AppHeaderItemLinkVue v-for="item in navMenu" :key="item.id" :text="item.text" />
+                    
+                </div>
+           </div>
+ 
+        </div>
+        
     </header>
 
 </template>
 
 <style lang="scss" scoped>
+@use "../style/general.scss" as *;
+@use "../style/partial/variables" as *;
+@use "../style/partial/mixin" as *;
+
+
+header {
+    background-image: url(../img/home-page-slider.jpg)
+}
+.nav {
+    @include flex();
+    .logo {
+        @include flex(row, flex-start, center, nowrap);
+        font-size: 0.8rem;
+        
+        
+    }
+
+    .nav-items {
+        @include flex();
+        gap: 1rem;
+        font-size: 0.8rem;
+    }
+}
+
 
 </style>
--- src/components/AppHeaderItemLink.vue
+++ src/components/AppHeaderItemLink.vue
@@ -13,11 +13,41 @@ export default {
 </script>
 
 <template>
+    <ul>
+        <li>
+           <a :href="` /${text }`"> {{ uppercaseText }} </a> 
+        </li>
+    </ul>
 
-    <a :href="` /${text }`"> {{ uppercaseText }} </a>
+    
 
 </template>
 
 <style lang="scss" scoped >
+@use "../style/partial/_mixin.scss" as *;
+@use "../style/partial/variables" as *;
+
+ul {
+    list-style-type: none;
+    li {
+        @include flex();
+    }
+    
+    a {
+   
+    color: $primary-gold;
+    @include flex();
+
+    
+    }
+
+
+
+
+    
+}
+
+
+
 
 </style>
