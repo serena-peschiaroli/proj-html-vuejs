d98346d364e7950996f0f71eddbe5674d388e8e6
serena.peschiaroli <serena.peschiaroli@gmail.com>
2023-12-05 12:52:19 +0100

    Add: listPhoto on Section three, wip

--- src/components/AppMainSectThree.vue
+++ src/components/AppMainSectThree.vue
@@ -1,16 +1,30 @@
 <script>
 import AppMainTitleVue from './AppMainTitle.vue';
+import AppSectThreePhotoList from './AppSectThreePhotoList.vue';
 
 export default {
     components: {
         AppMainTitleVue,
+        AppSectThreePhotoList,
     },
     data() {
         return {
             mainTitle: 'Main Title',
             subTitle: 'Sub Title',
+            imageListElements: [
+                {image: 'avada-charity-shelter-featured-200x150.jpg'},
+                {image: 'avada-charity-farming-featured-200x150.jpg'},
+                {image: 'avada-charity-vaccines-featured-200x150.jpg'},
+                {image: 'avada-charity-fair-trade-featured-200x150.jpg'},
+
+            ],
         }
     },
+    methods: {
+        getImagePath(image) {
+            return `/src/img/${image}`;
+        }
+    }
 }
 
 </script>
@@ -19,6 +33,10 @@ export default {
     <section>
         <div class="container-xl">
             <AppMainTitleVue :mainTitle="mainTitle" :subTitle="subTitle" :isCentered="true" />
+
+            <div class="list-element">
+                <AppSectThreePhotoList v-for="(image, index) in imageListElements" :key="index" :image="getImagePath(image.image)" />
+            </div>
 
         </div>
         <div class="red-stripe">
@@ -35,6 +53,13 @@ export default {
 
 section{
     .container-xl{
+
+        .list-element {
+            @include flex(row, center, center, nowrap);
+            gap: 1rem;
+            padding-bottom: 1rem;
+            margin-top: 1rem;
+        }
 
 
     }
---
+++ src/components/AppSectThreePhotoList.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    props: {
+        image: String,
+        text: String,
+
+    }
+
+}
+</script>
+
+<template>
+
+    <div class="img-element">
+        <div class="photo">
+            <img :src="image" alt="Photo">
+        </div>
+        <div class="text">
+             <p>{{ text }}</p>
+        </div>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+@use "../style/general.scss" as *;
+@use "../style/partial/mixin" as *;
+
+.img-element {
+    @include flex(row, center, center, nowrap);
+    .photo {
+        width: 100%;
+    }
+}
+
+</style>
